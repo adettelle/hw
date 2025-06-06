b275173fc232cdf316e0d7af0780ade4152520eb
adettelle <113937974+adettelle@users.noreply.github.com>
2025-06-06 16:21:38 +0300

    Hw02 unpack string (#2)
    * fix go.mod for task 1
    
    * HW2 is completed
    
    * delete sync files
    
    * fix module name

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	escapeMode := false
+	r := []rune(s)
+
+	var sb strings.Builder
+
+	for i := 0; i < len(r); i++ {
+		if !escapeMode && unicode.IsDigit(r[i]) { // первый элемент цифра
+			return "", ErrInvalidString
+		}
+
+		if i == len(r)-1 {
+			if string(r[i]) == `\` { // последний элемент `\`
+				return "", ErrInvalidString
+			}
+			// последний элемент любой другой
+			sb.WriteString(string(r[i]))
+			return sb.String(), nil
+		}
+		// ----------------- не последний элемент -----------------
+
+		if !escapeMode && string(r[i]) == `\` {
+			escapeMode = true
+			continue
+		}
+
+		if escapeMode && (!unicode.IsDigit(r[i]) && string(r[i]) != `\`) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(r[i+1]) { // `?4` следующий элемент - цифра
+			n, err := strconv.Atoi(string(r[i+1]))
+			if err != nil {
+				return "", err
+			}
+
+			sb.WriteString(strings.Repeat(string(r[i]), n))
+
+			i++
+		} else { // `ab` следующий элемент - не цифра
+			sb.WriteString(string(r[i]))
+			escapeMode = false
+		}
+	}
+	return sb.String(), nil
 }
