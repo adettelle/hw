5ac4446767966e698e4a3b0b1f07c839b6246d4a
adettelle <113937974+adettelle@users.noreply.github.com>
2025-06-11 20:07:04 +0300

    HW3 is completed (#3)
    * HW3 is completed
    
    * fix linter error: omit type from declaration of var
    
    * remove unneeded condition

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,61 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var re = regexp.MustCompile(`^["',.!?:;]+|["',.!?:;]+$`)
+
+type keyValue struct {
+	Key   string
+	Value int
+}
+
+func Top10(str string) []string {
+	words := strings.Fields(str)
+	wordFrequencies := make(map[string]int)
+
+	if len(str) == 0 {
+		return nil
+	}
+
+	for _, w := range words {
+		word := strings.ToLower(w)
+		if word == "-" {
+			continue
+		}
+		reWord := re.ReplaceAllString(word, "")
+
+		wordFrequencies[reWord]++
+	}
+
+	sortedFrequeencies := getSortedFrequeencies(wordFrequencies, 10)
+
+	result := make([]string, 0, len(sortedFrequeencies))
+
+	for _, pair := range sortedFrequeencies {
+		result = append(result, pair.Key)
+	}
+
+	return result
+}
+
+// returns "amount" elements if available (or amount = length of result slice).
+func getSortedFrequeencies(wordFrequencies map[string]int, amount int) []keyValue {
+	result := make([]keyValue, 0, len(wordFrequencies))
+
+	for key, value := range wordFrequencies {
+		result = append(result, keyValue{key, value})
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		if result[i].Value == result[j].Value {
+			return result[i].Key < result[j].Key
+		}
+		return result[i].Value > result[j].Value
+	})
+
+	return result[:min(amount, len(result))]
 }
