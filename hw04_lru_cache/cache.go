fa57c6db4f81c52ccf32ae571eeff5d546397853
Liudmila <l.litvinski@gmail.com>
2025-06-16 17:22:01 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,25 +1,97 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
-type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
-	Clear()
+type IList interface {
+	Len() int
+	Front() *ListItem
+	Back() *ListItem
+	PushFront(v interface{}) *ListItem
+	PushBack(v interface{}) *ListItem
+	Remove(i *ListItem)
+	MoveToFront(i *ListItem)
 }
 
-type lruCache struct {
-	Cache // Remove me after realization.
-
+type LruCache struct {
+	sync.RWMutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-func NewCache(capacity int) Cache {
-	return &lruCache{
+func NewCache(capacity int) *LruCache {
+	return &LruCache{
 		capacity: capacity,
-		queue:    NewList(),
+		queue:    *NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+type pair struct {
+	key   Key // interface{}
+	value interface{}
+}
+
+// Добавить значение в кэш по ключу
+// если уже есть, заменить на новое значение.
+func (c *LruCache) Set(key Key, value interface{}) bool {
+	c.Lock()
+	defer c.Unlock()
+
+	node, ok := c.items[key]
+
+	if ok {
+		node.Value = pair{key: key, value: value}
+		c.queue.MoveToFront(node)
+		return true
+	}
+
+	kv := pair{key: key, value: value}
+
+	li := c.queue.PushFront(kv)
+	c.items[key] = li
+
+	if c.queue.length > c.capacity {
+		valueTodelete := c.queue.tail
+		c.queue.Remove(valueTodelete)
+
+		val, ok := valueTodelete.Value.(pair)
+		if !ok {
+			panic("Unexpected val type")
+		}
+
+		delete(c.items, val.key)
+	}
+
+	return false
+}
+
+// Получить значение из кэша по ключу.
+func (c *LruCache) Get(key Key) (interface{}, bool) {
+	c.RLock()
+	defer c.RUnlock()
+
+	node, ok := c.items[key]
+
+	if ok {
+		c.queue.MoveToFront(node)
+		val, ok := node.Value.(pair)
+		if !ok {
+			panic("Unexpected value type")
+		}
+		return val.value, true
+	}
+	return nil, false
+}
+
+func (c *LruCache) Clear() {
+	c.Lock()
+	defer c.Unlock()
+	c.queue.DeleteLinkedList()
+}
+
+func (c *LruCache) PrintList() {
+	c.queue.printList()
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,14 +1,6 @@
 package hw04lrucache
 
-type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
-}
+import "fmt"
 
 type ListItem struct {
 	Value interface{}
@@ -16,11 +8,149 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
-type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+type List struct {
+	head   *ListItem
+	tail   *ListItem
+	length int
 }
 
-func NewList() List {
-	return new(list)
+func NewList() *List {
+	return &List{}
+}
+
+func (list *List) Len() int {
+	return list.length
+}
+
+func (list *List) Front() *ListItem {
+	return list.head
+}
+
+func (list *List) Back() *ListItem {
+	return list.tail
+}
+
+// Добавить значение в начало.
+func (list *List) PushFront(v interface{}) *ListItem {
+	i := ListItem{Value: v}
+
+	if list.head == nil {
+		list.head = &i
+		list.tail = &i
+		i.Prev = nil
+		i.Next = nil
+	} else {
+		list.head.Prev = &i
+		i.Next = list.head
+		list.head = &i
+	}
+	list.length++
+
+	return &i
+}
+
+// Добавить значение в конец.
+func (list *List) PushBack(v interface{}) *ListItem {
+	i := ListItem{Value: v}
+
+	if list.head == nil {
+		list.head = &i
+		list.tail = &i
+		i.Prev = nil
+		i.Next = nil
+	} else {
+		list.tail.Next = &i
+		i.Prev = list.tail
+		list.tail = &i
+	}
+	list.length++
+
+	return &i
+}
+
+// Удалить элемент.
+func (list *List) Remove(i *ListItem) {
+	if list.head == nil {
+		return
+	}
+
+	if list.head == i {
+		list.head = list.head.Next
+		list.head.Prev = nil
+		list.length--
+		return
+	}
+	if list.tail == i {
+		list.tail = list.tail.Prev
+		list.tail.Next = nil
+		list.length--
+		return
+	}
+	temp := list.head.Next
+
+	for temp != nil {
+		if temp == i {
+			temp.Prev.Next = temp.Next
+			temp.Next.Prev = temp.Prev.Next
+			list.length--
+			return
+		}
+		temp = temp.Next
+	}
+}
+
+// Переместить элемент в начало.
+func (list *List) MoveToFront(i *ListItem) {
+	if i == list.head {
+		return
+	}
+
+	temp := list.head.Next // temp - второй элемент в списке
+
+	for temp != nil {
+		if temp == i {
+			temp.Prev.Next = temp.Next
+			if temp.Next != nil {
+				temp.Next.Prev = temp.Prev
+			}
+			if temp == list.tail {
+				list.tail = temp.Prev
+			}
+
+			list.head.Prev = temp
+			temp.Next = list.head
+			list.head = temp
+			temp.Prev = nil
+
+			return
+		}
+		temp = temp.Next
+	}
+}
+
+func (list *List) DeleteLinkedList() {
+	current := list.head
+	for current != nil {
+		current.Prev = nil
+		temp := current
+		current = current.Next
+		temp.Next = nil
+	}
+	list.head = nil
+	list.length = 0
+
+	list.tail = nil
+}
+
+func (list *List) printList() {
+	if list.head == nil {
+		fmt.Println("Empty Linked List")
+	} else {
+		temp := list.head
+		for temp != nil {
+			fmt.Printf("%v <-> ", temp.Value)
+			temp = temp.Next
+		}
+	}
+	fmt.Println()
 }
