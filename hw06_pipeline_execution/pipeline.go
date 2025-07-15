4d91233ab699fbcef9e78639fb77a4614c305c8d
Liudmila <l.litvinski@gmail.com>
2025-07-15 20:50:40 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,41 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	currentIn := in
+
+	for _, stage := range stages {
+		wrapped := createClosableChan(currentIn, done)
+		currentIn = stage(wrapped)
+	}
+
+	return currentIn
+}
+
+func createClosableChan(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case _, ok := <-done:
+				if !ok {
+					return
+				}
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- val
+			}
+		}
+	}()
+
+	return out
 }
