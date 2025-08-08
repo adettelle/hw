b023c436661a5fd18c8da725e6186c54afb186fa
Liudmila <l.litvinski@gmail.com>
2025-08-08 19:49:23 +0300

    HW9 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,291 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+func (ve *ValidationError) Error() string {
+	return fmt.Sprintf("Error in filed: %v, err: %v", ve.Field, ve.Err)
+}
+
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	x := []string{}
+	for _, val := range v {
+		x = append(x, val.Error())
+	}
+	slices.Sort(x)
+	return strings.Join(x, "; ")
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	errs := ValidationErrors{}
+	val := reflect.ValueOf(v)
+	typeInfo := reflect.TypeOf(v)
+
+	// Пробегаемся по полям структуры
+	for fieldIndex := 0; fieldIndex < val.NumField(); fieldIndex++ {
+		fieldInfo := typeInfo.Field(fieldIndex)
+
+		// проверяем, экспортируемое ли поле
+		if !fieldInfo.IsExported() {
+			continue
+		}
+
+		tag := fieldInfo.Tag.Get("validate")
+		if tag == "" {
+			continue
+		}
+
+		// Получаем значение поля структуры, напимер, возраст 19
+		checkedValue := val.Field(fieldIndex)
+
+		constraints, err := parseTagFuncs(tag) // ранее tagFuncs
+		if err != nil {
+			errs = append(errs, ValidationError{Field: fieldInfo.Name, Err: err})
+		}
+
+		// проверяем, какое поле int или string (25 - Int)
+		// TODO надо ли проверять float???
+		switch checkedValue.Kind() {
+		case reflect.String:
+			err := validateString(checkedValue, constraints) // , validErrs, field
+			if err != nil {
+				errs = append(errs, ValidationError{Field: fieldInfo.Name, Err: err})
+			}
+
+		case reflect.Int:
+			err := validateInt(checkedValue, constraints) //  checkRestrictionsInt(tFuncs, int(checkedValue.Int()))
+			if err != nil {
+				errs = append(errs, ValidationError{Field: fieldInfo.Name, Err: err})
+			}
+
+		case reflect.Slice:
+			data := checkedValue.Slice(0, checkedValue.Len())
+			validateErrs := validateSlice(constraints, data, fieldInfo.Name)
+			if len(validateErrs) != 0 {
+				errs = append(errs, validateErrs...)
+			}
+		}
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
 	return nil
+}
+
+func validateString(checkedValue reflect.Value, constraints map[string][]string) error {
+	for k, v := range constraints {
+		fmt.Println("K AND V", k, v)
+		switch k {
+		case "len":
+			value, err := strconv.Atoi(v[0])
+			if err != nil {
+				return err
+			}
+			if checkedValue.Len() != value {
+				return NewErrCheckedValueLen(value)
+			}
+		case "regexp":
+			re, err := regexp.Compile(v[0])
+			if err != nil {
+				return err
+			}
+			ok := re.MatchString(checkedValue.String())
+			if !ok {
+				return NewErrMatch(v[0])
+			}
+		case "in":
+			in := false
+			for _, word := range v {
+				if checkedValue.String() == word {
+					in = true
+				}
+			}
+			if !in {
+				return NewErrNotInRange(v)
+			}
+		}
+	}
+	return nil
+}
+
+func validateInt(checkedValue reflect.Value, constraints map[string][]string) error {
+	for k, v := range constraints {
+		switch k {
+		case "min":
+			minValue, err := strconv.Atoi(v[0])
+			if err != nil {
+				return err
+			}
+			if int(checkedValue.Int()) < minValue {
+				return NewErrValueTooSmall(minValue)
+			}
+		case "max":
+			maxValue, err := strconv.Atoi(v[0])
+			if err != nil {
+				return err
+			}
+			if int(checkedValue.Int()) > maxValue {
+				return NewErrValueTooBig(maxValue)
+			}
+		case "in":
+			found := false
+			for _, val := range v {
+				num, err := strconv.Atoi(val)
+				if err != nil {
+					return err
+				}
+				if num == int(checkedValue.Int()) {
+					found = true
+				}
+			}
+			if !found {
+				return NewErrNotInRange(v)
+			}
+		}
+	}
+	return nil
+}
+
+func validateSlice(constraints map[string][]string,
+	checkedValue reflect.Value, fieldName string) ValidationErrors {
+	validErrs := ValidationErrors{}
+	if checkedValue.Len() == 0 {
+		for k, v := range constraints {
+			num, err := strconv.Atoi(v[0])
+			if err != nil {
+				return ValidationErrors{ValidationError{Err: err}}
+			}
+			switch k {
+			case "len":
+				err = NewErrCheckedValueLen(num)
+				ve := ValidationError{Field: fieldName, Err: err}
+				validErrs = append(validErrs, ve)
+			case "min":
+				err := NewErrValueTooSmall(num)
+				ve := ValidationError{Field: fieldName, Err: err}
+				validErrs = append(validErrs, ve)
+			case "max":
+				err := NewErrValueTooBig(num)
+				ve := ValidationError{Field: fieldName, Err: err}
+				validErrs = append(validErrs, ve)
+			}
+		}
+	}
+
+	for fieldIndex := 0; fieldIndex < checkedValue.Len(); fieldIndex++ {
+		valToCheck := checkedValue.Index(fieldIndex)
+
+		switch valToCheck.Kind() {
+		case reflect.String:
+			err := validateString(valToCheck, constraints)
+			if err != nil {
+				validErrs = append(validErrs, ValidationError{Field: fieldName, Err: err})
+			}
+		case reflect.Int:
+
+			err := validateInt(valToCheck, constraints) // checkRestrictionsInt(tagFuncs, int(checkedValue.Int()))
+			if err != nil {
+				validErrs = append(validErrs, ValidationError{Field: fieldName, Err: err})
+			}
+		}
+	}
+	if len(validErrs) == 0 {
+		return nil
+	}
+	return validErrs
+}
+
+var (
+	ErrConditionsRepeat = errors.New("conditions are repeated")
+)
+
+type ErrValueTooBig struct {
+	expectedMax int
+}
+
+func NewErrValueTooBig(expectedMax int) *ErrValueTooBig {
+	return &ErrValueTooBig{expectedMax: expectedMax}
+}
+
+func (err *ErrValueTooBig) Error() string {
+	return fmt.Sprintf("checkedValue should be less than: %v", err.expectedMax)
+}
+
+type ErrValueTooSmall struct {
+	expectedMin int
+}
+
+func NewErrValueTooSmall(expectedMin int) *ErrValueTooSmall {
+	return &ErrValueTooSmall{expectedMin: expectedMin}
+}
+
+func (err *ErrValueTooSmall) Error() string {
+	return fmt.Sprintf("checkedValue should be grater than: %v", err.expectedMin)
+}
+
+type ErrNotInRange struct {
+	limit []string
+}
+
+func NewErrNotInRange(limit []string) *ErrNotInRange {
+	return &ErrNotInRange{limit: limit}
+}
+
+func (err *ErrNotInRange) Error() string {
+	return fmt.Sprintf("checkedValue should be in range: %v", err.limit)
+}
+
+type ErrMatch struct {
+	re string
+}
+
+func NewErrMatch(re string) *ErrMatch {
+	return &ErrMatch{re: re}
+}
+
+func (err *ErrMatch) Error() string {
+	return fmt.Sprintf("checkedValue should match regular expression: %v", err.re)
+}
+
+type ErrCheckedValueLen struct {
+	length int
+}
+
+func NewErrCheckedValueLen(length int) *ErrCheckedValueLen {
+	return &ErrCheckedValueLen{length: length}
+}
+
+func (e *ErrCheckedValueLen) Error() string {
+	return fmt.Sprintf("wrong length of checkedValue, %v", e.length)
+}
+
+func parseTagFuncs(tag string) (map[string][]string, error) {
+	funcsMap := map[string][]string{}
+	funcs := strings.Split(tag, "|")
+
+	for _, val := range funcs {
+		elem := strings.Split(val, ":")
+		_, ok := funcsMap[elem[0]]
+		if ok {
+			return nil, ErrConditionsRepeat
+		}
+		limits := strings.Split(elem[1], ",")
+		funcsMap[elem[0]] = limits
+	}
+	return funcsMap, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -18,6 +20,12 @@ type (
 		Role   UserRole        `validate:"in:admin,stuff"`
 		Phones []string        `validate:"len:11"`
 		meta   json.RawMessage //nolint:unused
+	}
+
+	Student struct {
+		Name     string
+		Age      int   `validate:"max:80"`
+		AvRating []int `validate:"len:6|min:3|max:5"`
 	}
 
 	App struct {
@@ -36,16 +44,96 @@ type (
 	}
 )
 
-func TestValidate(t *testing.T) {
+func TestValidatePositive(t *testing.T) {
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "Ane",
+				Email:  "ane@gmail.com",
+				Age:    19,
+				Role:   "stuff",
+				Phones: []string{"12345678901"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "Alex",
+				Age:    18,
+				Email:  "alex@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "12345678901"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
+		{
+			in: Student{
+				Age:      10,
+				AvRating: []int{5, 5, 3, 3, 4, 4},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
+		{
+			in: Student{
+				Name:     "Alex",
+				AvRating: []int{5, 5, 5, 5, 4, 4},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
+		{
+			in: App{
+				Version: "abcde",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
+		{
+			in: Token{
+				Header:    []byte(""),
+				Payload:   []byte(""),
+				Signature: []byte(""),
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
+		{
+			in: Response{
+				Code: 404,
+				Body: "",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Err: nil,
+				},
+			},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +141,174 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestValidateNegative(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Age:    20,
+				Email:  "mary@gmail.com",
+				Phones: []string{"1234", "12345678901"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Role",
+					Err:   NewErrNotInRange([]string{"admin", "stuff"}),
+				},
+				ValidationError{
+					Field: "Phones",
+					Err:   NewErrCheckedValueLen(11),
+				},
+			},
+		},
+		{
+			in: Student{
+				Age: 91,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Age",
+					Err:   NewErrValueTooBig(80),
+				},
+				ValidationError{
+					Field: "AvRating",
+					Err:   NewErrCheckedValueLen(6),
+				},
+				ValidationError{
+					Field: "AvRating",
+					Err:   NewErrValueTooSmall(3),
+				},
+				ValidationError{
+					Field: "AvRating",
+					Err:   NewErrValueTooBig(5),
+				},
+			},
+		},
+		{
+			in: User{
+				Name:   "Ane",
+				Email:  "anegmail.com",
+				Age:    19,
+				Phones: []string{"1234", "12345678901"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "ID",
+					Err:   NewErrCheckedValueLen(36),
+				},
+				ValidationError{
+					Field: "Email",
+					Err:   NewErrMatch("^\\w+@\\w+\\.\\w+$"),
+				},
+				ValidationError{
+					Field: "Role",
+					Err:   NewErrNotInRange([]string{"admin", "stuff"}),
+				},
+				ValidationError{
+					Field: "Phones",
+					Err:   NewErrCheckedValueLen(11),
+				},
+			},
+		},
+		{
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "Alex",
+				Age:    17,
+				Email:  "alex@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "12345"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Age",
+					Err:   NewErrValueTooSmall(18),
+				},
+				ValidationError{
+					Field: "Phones",
+					Err:   NewErrCheckedValueLen(11),
+				},
+			},
+		},
+		{
+			in: App{
+				Version: "abcdefg",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Version",
+					Err:   NewErrCheckedValueLen(5),
+				},
+			},
+		},
+		{
+			in: Response{
+				Code: 400,
+				Body: "",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Code",
+					Err:   NewErrNotInRange([]string{"200", "404", "500"}),
+				},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+			require.Error(t, err)
+			require.Equal(t, tt.expectedErr.Error(), err.Error())
+		})
+	}
+}
+
+func TestArrangeTagFuncs(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected map[string][]string
+	}{
+		{
+			name:     "check one condition",
+			input:    "len:36",
+			expected: map[string][]string{"len": {"36"}},
+		},
+		{
+			name:     "check two conditions",
+			input:    "min:18|max:50",
+			expected: map[string][]string{"min": {"18"}, "max": {"50"}},
+		},
+		{
+			name:     "condition with backslash",
+			input:    "regexp:^\\w+@\\w+\\.\\w+$",
+			expected: map[string][]string{"regexp": {"^\\w+@\\w+\\.\\w+$"}},
+		},
+		{
+			name:     "condition with slice in value",
+			input:    "in:admin,stuff",
+			expected: map[string][]string{"in": {"admin", "stuff"}},
+		},
+	}
+
+	for _, tCase := range tests {
+		t.Run(tCase.name, func(t *testing.T) {
+			result, err := parseTagFuncs(tCase.input)
+			require.NoError(t, err)
+			require.Equal(t, tCase.expected, result)
 		})
 	}
 }
